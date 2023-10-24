8c77d1fc38d640e7286980704f8337e11ad0a13c
Diego RUIZ PONSODA <diego.ruiz-ponsoda@student-cs.fr>
2023-10-24 15:03:25 +0200

    Test sim 4

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
     const simscore= similarity("Chaud","Froid");
-    return new Response("Hello Score ", simscore);
+    return new Response("Hello Score " + JSON.stringify(simscore));
 }
 
 async function similarity(word1:string , word2:string) {
