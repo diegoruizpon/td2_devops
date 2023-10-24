50424f85618c61902d5104ed0d6289fb00eb81e1
Diego RUIZ PONSODA <diego.ruiz-ponsoda@student-cs.fr>
2023-10-24 14:57:15 +0200

    Test sim 2

--- index.ts
+++ index.ts
@@ -1,14 +1,38 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-    const conc_words = similarity("Hola","Mundo");
-    return new Response("Hello World" + conc_words);
+    const conc_words = similarity("Chaud","Froid");
+    return new Response("Hello World" + JSON.stringify(conc_words));
 }
 
-function similarity(word1:string , word2:string) {
-    
-    return (word1+word2);
-    
+async function similarity(word1:string , word2:string) {
+
+    const response = await fetch("http://nlp.polytechnique.fr/similarityscore",{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            sim1: word1,
+            sim2: word2,
+            lang: 'fr',
+            type: 'General Word2Vec'
+
+        }),
+    });
+
+    const similarityResponseJson = await response.json();
+
+    return (similarityResponseJson.simscore);
 }
+    
+    
+
+    
+    
+    
+
+
+
 
 serve(handler);
