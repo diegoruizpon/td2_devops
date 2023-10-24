88298455cbb1b5b50bc915642364172aa4afa0fe
Diego RUIZ PONSODA <diego.ruiz-ponsoda@student-cs.fr>
2023-10-24 14:23:05 +0200

    Test sim

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+    const conc_words = similarity("Hola","Mundo");
+    return new Response("Hello World" + conc_words);
+}
+
+function similarity(word1:string , word2:string) {
+    
+    return (word1+word2);
+    
 }
 
 serve(handler);
